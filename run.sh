d1fa6c2ba07883bac566d6e71001fabe63575b40
Mark Alston <malston@pivotal.io>
2016-07-25 10:46:05 -0400

    Fix sonar_url property

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # TODO --informative-errors does not seem to be available in this version of Git
-git daemon --verbose --enable=receive-pack --base-path=/tmp/files --export-all &
+# git daemon --verbose --enable=receive-pack --base-path=/tmp/files --export-all &
 
 # TODO without this JENKINS-24752 workaround, it takes too long to provision.
 # (Do not add hudson.model.LoadStatistics.decay=0.1; in that case we overprovision slaves which never get used, and OnceRetentionStrategy.check disconnects them after an idle timeout.)
